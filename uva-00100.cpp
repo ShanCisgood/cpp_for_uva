b8efce59e82839a6d800c700a7a95b8217535b88
ShanC <shane931113@gmail.com>
2023-11-17 00:03:57 +0800

    Update uva-00100.cpp

--- uva-00100.cpp
+++ uva-00100.cpp
@@ -1,16 +1,26 @@
 #include <bits/stdc++.h>
-#define LL long long
 using namespace std;
-int func(LL n) {}
+int func(int n) {
+    int cnt = 1;
+    while (n - 1) {
+        n = (n & 1 ? 3 * n + 1 : (n >> 1));
+        cnt++;
+    }
+    return cnt;
+}
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
     int a, b;
     while (cin >> a >> b) {
-        for (LL i = a; i <= b; i++) {
-            cout << a << ' ' << b << ' ' << func(i) << '\n';
-        }
+        int mx = 0;
+        cout << a << ' ' << b << ' ';
+        if (a > b)
+            swap(a, b);
+        for (int i = a; i <= b; i++)
+            mx = max(mx, func(i));
+        cout << mx << '\n';
     }
 
     return 0;
