a6d8c95dfb90dd1b5e9da0ed2e189a644d1a9c39
ShanC <shane931113@gmail.com>
2023-11-20 23:25:13 +0800

    10268 AC

---
+++ uva-10268.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define ll long long
+int arr[1000000], x;
+using namespace std;
+int main() {
+    while (cin >> x) {
+        int n, exp = 1;
+        ll ans = 0;
+        for (n = 0;; n++) {
+            scanf("%d", &arr[n]);
+            if (getchar() == '\n')
+                break;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            ans += arr[i] * exp * (n - i);
+            exp *= x;
+        }
+        printf("%d\n", ans);
+    }
+    return 0;
+}
