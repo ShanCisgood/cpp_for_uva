4e1911050815ecf541d121c68bf1bc40d73a2acd
ShanC <shane931113@gmail.com>
2024-08-30 00:50:43 +0800

    AC again

--- vol 00/uva-00438.cpp
+++ vol 00/uva-00438.cpp
@@ -20,8 +20,8 @@ int main() {
         Pt b = {pts[2].x - pts[0].x, pts[2].y - pts[0].y};
         Pt c = {pts[2].x - pts[1].x, pts[2].y - pts[1].y};
         double sint = area(a, b) / sqrt(a.x * a.x + a.y * a.y) / sqrt(b.x * b.x + b.y * b.y);
-        double r = sqrt(c.x * c.x + c.y * c.y) / 2.0 / sint;
-        cout << 2.0 * pi * r << '\n';
+        double r = sqrt(c.x * c.x + c.y * c.y) / sint;
+        cout << pi * r << '\n';
     }
 
     return 0;
