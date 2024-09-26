0e646d77ca6e33031951a045551450081857c8fd
ShanC <shane931113@gmail.com>
2024-09-26 15:38:40 +0800

    fixed dump bugs

--- vol 00/uva-00100.cpp
+++ vol 00/uva-00100.cpp
@@ -1,14 +1,15 @@
 #include <bits/stdc++.h>
+#define int long long
 using namespace std;
 int func(int n) {
     int cnt = 1;
-    while (n - 1) {
-        n = (n & 1 ? 3 * n + 1 : n >> 2);
+    while (n != 1) {
+        n = (n & 1 ? 3 * n + 1 : n / 2);
         cnt++;
     }
     return cnt;
 }
-int main() {
+signed main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
 
@@ -18,8 +19,10 @@ int main() {
         cout << a << ' ' << b << ' ';
         if (a > b)
             swap(a, b);
-        for (int i = a; i <= b; i++)
+        for (int i = a; i <= b; i++) {
+            // cout << i << ": " << func(i) << '\n';
             mx = max(mx, func(i));
+        }
         cout << mx << '\n';
     }
 
