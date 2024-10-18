64421d61d8040ea7ac39fb28a38910b3ec773b5c
ShanC <shane931113@gmail.com>
2024-10-18 22:04:28 +0800

    10110 OK

--- vol 10/uva-10110.cpp
+++ vol 10/uva-10110.cpp
@@ -8,15 +8,11 @@ int main() {
 
     ll n;
     while (cin >> n && n) {
-        int cnt = 1 + (n != 1);
-        for (ll i = 2; i * i <= n; i++) {
-            if (n % i == 0) {
-                cnt++;
-                if (n != i * i)
-                    cnt++;
-            }
-        }
-        cout << (cnt % 2 == 1 ? "yes" : "no") << '\n';
+        ll k = sqrt(n);
+        if (k * k == n)
+            cout << "yes\n";
+        else
+            cout << "no\n";
     }
 
     return 0;
