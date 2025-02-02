d8c878f1ed588fd03201595be76e546338d319fb
ShanC <shane931113@gmail.com>
2025-02-02 08:20:27 +0800

    XD

--- tmp2.py
+++
@@ -1,4 +0,0 @@
-def func(ls):
-    return ls[0] <= 20 and ls[1] <= 20 and ls[2] <= 20 
-for i in range(1, int(input()) + 1):
-    print(f'Case {i}: {"good" if func(list(map(int, input().split()))) else "bad"}')
